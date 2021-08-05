78f7bb36c394f6e8500c4f198f0ef872bc074aff
neko113 <alstn113@gmail.com>
2021-08-05 15:33:53 +0900

    Register

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { createLogger } from "redux-logger";
 import ReduxThunk from "redux-thunk";
 import rootReducer from "./modules";
 import "./index.css";
 import App from "./App";
 
-const logger = createLogger();
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, ReduxThunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,14 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import createRequestThunk, { createRequestActionTypes } from "../lib/createRequestThunk";
+import * as authAPI from "../lib/api/auth";
 
 const CHANGE_FIELD = "auth/CHANGE_FIELD";
 const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes("auth/REGISTER");
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes("auth/LOGIN");
 
 export const changeField = createAction(CHANGE_FIELD, ({ form, key, value }) => ({
   form,
@@ -11,7 +17,11 @@ export const changeField = createAction(CHANGE_FIELD, ({ form, key, value }) => 
 }));
 export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
 
-const initializeState = {
+export const register = createRequestThunk(REGISTER, authAPI.register);
+
+export const login = createRequestThunk(LOGIN, authAPI.login);
+
+const initialState = {
   register: {
     username: "",
     password: "",
@@ -21,20 +31,40 @@ const initializeState = {
     username: "",
     password: "",
   },
+  auth: null,
+  authError: null,
 };
 
 const auth = handleActions(
   {
     [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
       produce(state, (draft) => {
-        draft[form][key] = value;
+        draft[form][key] = value; // 예: state.register.username을 바꾼다
       }),
-    [INITIALIZE_FORM]: (state, { payload: { form } }) => ({
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
-      [form]: initializeState[form],
+      [form]: initialState[form],
+    }),
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
+    }),
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: error,
     }),
   },
-  initializeState
+  initialState
 );
 
 export default auth;
